1cb80b301d7950b1b73021ca1abff28775091347
Michele Rawlins <shellbop@att.net>
2020-02-22 13:20:52 -0600

    added mouse over

--- main.js
+++ main.js
@@ -74,8 +74,10 @@ const singleDinoAddEvents=()=>{
 const dinoHealth = (e) => {
     const dinoId = e.target.closest('.card').id;
     const dinoPosition = dinos.findIndex((p) => p.id === dinoId);
+    if(dinos[dinoPosition].health  < 100){
  dinos[dinoPosition].health += 1;
  printDinos(dinos);
+}
      };
 
      
